226f50d61587f12ae7d8871eb28cb6abda11c1bc
Rita Zevallos <rzevall1@swarthmore.edu>
2014-06-30 23:07:13 -0400

    added update

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,7 +5,7 @@ class BooksController < ApplicationController
 		@book = @category.books.create(book_params)
 		@page = MetaInspector.new(@book.link)
 		@book.title = @page.title.strip
-		@book.description = @page.description
+		@book.description = @page.description.strip
 		respond_to do |format|
 			if @book.save
 	          format.html {redirect_to categories_path, notice: 'Book created.'}
@@ -15,6 +15,29 @@ class BooksController < ApplicationController
 	    		format.js
 	    	end
       	end
+	end
+
+	def edit
+	  	@book = Book.find(params[:id])
+		respond_to do |format|
+		  	format.html
+		  	format.js
+	  	end
+	end
+
+	def update
+		@categories = Category.all
+		@books = Book.all
+		@book = Book.find(params[:id])
+		respond_to do |format|
+			if @book.update_attributes(book_edit_params)
+				format.html {redirect_to categories_path, notice: 'Book edited.'}
+				format.js
+			else
+				format.html {redirect_to categories_path, notice: 'Error.'}
+				format.js
+			end
+		end
 	end
 
 	def destroy
@@ -40,5 +63,9 @@ class BooksController < ApplicationController
 
 	def book_params
 		params.require(:book).permit(:link)
+	end
+
+	def book_edit_params
+		params.require(:book).permit(:title,:link,:description)
 	end
 end
